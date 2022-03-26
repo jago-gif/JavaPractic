7b3b5f02cd8ac7578531287a344268bed57a9bc9
jago <halcon1411@gmail.com>
2022-03-26 14:55:18 -0300

    cambiando el branch

--- src/cl/ejercicio1/Prueba.java
+++ src/cl/ejercicio1/Prueba.java
@@ -3,5 +3,6 @@ package cl.ejercicio1;
 public class Prueba {
     public static void main(String[] args) {
         System.out.println("esto es una prueba");
+        System.out.println("cambiando el branch");
     }
 }
